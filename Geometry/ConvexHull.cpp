55e3792bbbde12e0387b69a896dce02e38bdca9c
int0x00 <int0x00@users.noreply.github.com>
2014-11-16 22:18:08 +0100

    Modify ConvexHull and add GeometryMiscellaneous from stanford notebook. Delete duplicated functions.

--- Geometry/ConvexHull.cpp
+++ Geometry/ConvexHull.cpp
@@ -1,67 +1,80 @@
+// Compute the 2D convex hull of a set of points using the monotone chain
+// algorithm.  Eliminate redundant points from the hull if REMOVE_REDUNDANT is 
+// #defined.
+//
+// Running time: O(n log n)
+//
+//   INPUT:   a vector of input points, unordered.
+//   OUTPUT:  a vector of points in the convex hull, counterclockwise, starting
+//            with bottommost/leftmost point
+#include <iostream>
+#include <cstdio>
+#include <cassert>
 #include <vector>
 #include <algorithm>
-#include <math.h>
-
-typedef struct { int x; int y;} point;
+#include <cmath>
 
 using namespace std;
 
-point pivot; // global variable
-vector<point> polygon, CH;
+#define REMOVE_REDUNDANT
 
-int area2(point a, point b, point c) {
-    return a.x * b.y - a.y * b.x + b.x * c.y - b.y * c.x + c.x * a.y - c.y * a.x;
+typedef double T;
+const T EPS = 1e-7;
+struct PT { 
+  T x, y; 
+  PT() {} 
+  PT(T x, T y) : x(x), y(y) {}
+  bool operator<(const PT &rhs) const { return make_pair(y,x) < make_pair(rhs.y,rhs.x); }
+  bool operator==(const PT &rhs) const { return make_pair(y,x) == make_pair(rhs.y,rhs.x); }
+};
+
+T cross(PT p, PT q) { return p.x*q.y-p.y*q.x; }
+T area2(PT a, PT b, PT c) { return cross(a,b) + cross(b,c) + cross(c,a); }
+
+#ifdef REMOVE_REDUNDANT
+bool between(const PT &a, const PT &b, const PT &c) {
+  return (fabs(area2(a,b,c)) < EPS && (a.x-b.x)*(c.x-b.x) <= 0 && (a.y-b.y)*(c.y-b.y) <= 0);
+}
+#endif
+
+void ConvexHull(vector<PT> &pts) {
+  sort(pts.begin(), pts.end());
+  pts.erase(unique(pts.begin(), pts.end()), pts.end());
+  vector<PT> up, dn;
+  for (int i = 0; i < pts.size(); i++) {
+    while (up.size() > 1 && area2(up[up.size()-2], up.back(), pts[i]) >= 0) up.pop_back();
+    while (dn.size() > 1 && area2(dn[dn.size()-2], dn.back(), pts[i]) <= 0) dn.pop_back();
+    up.push_back(pts[i]);
+    dn.push_back(pts[i]);
+  }
+  pts = dn;
+  for (int i = (int) up.size() - 2; i >= 1; i--) pts.push_back(up[i]);
+  
+#ifdef REMOVE_REDUNDANT
+  if (pts.size() <= 2) return;
+  dn.clear();
+  dn.push_back(pts[0]);
+  dn.push_back(pts[1]);
+  for (int i = 2; i < pts.size(); i++) {
+    if (between(dn[dn.size()-2], dn[dn.size()-1], pts[i])) dn.pop_back();
+    dn.push_back(pts[i]);
+  }
+  if (dn.size() >= 3 && between(dn.back(), dn[0], dn[1])) {
+    dn[0] = dn.back();
+    dn.pop_back();
+  }
+  pts = dn;
+#endif
 }
 
-int dist2(point a, point b) { // function to compute distance between 2 points
-    int dx = a.x - b.x, dy = a.y - b.y;
-    return dx * dx + dy * dy;
-}
-
-bool angle_cmp(point a, point b) { // important angle-sorting function
-    if (area2(pivot, a, b) == 0) // collinear
-        return dist2(pivot, a) < dist2(pivot, b); // which one closer
-    int d1x = a.x - pivot.x, d1y = a.y - pivot.y;
-    int d2x = b.x - pivot.x, d2y = b.y - pivot.y;
-    return (atan2((double)d1y, (double)d1x) - atan2((double)d2y, (double)d2x)) < 0;
-}
-
-vector<point> GrahamScan(vector<point> Polygon) {
-// first, find P0 = point with lowest Y and if tie: rightmost X
-    int i, P0 = 0, N = Polygon.size();
-    for (i = 1; i < N; i++)
-        if (Polygon[i].y < Polygon[P0].y ||
-                (Polygon[i].y == Polygon[P0].y && Polygon[i].x > Polygon[P0].x))
-            P0=i;
-    point temp = Polygon[0]; // swap selected vertex with Polygon[0]
-    Polygon[0] = Polygon[P0];
-    Polygon[P0] = temp;
-// second, sort points by angle w.r.t. P0, skipping Polygon [0]
-    pivot = Polygon[0]; // use this global variable as reference
-    sort(++Polygon.begin(), Polygon.end(), angle_cmp);
-// third, the ccw tests
-    stack<point> S;
-    point prev, now;
-    S.push(Polygon[N - 1]); // put two starting vertices into stack S
-    S.push(Polygon[0]);
-    i = 1; // and start checking the rest
-    while (i < N) { // note: N must be >= 3 for this method to work
-        now = S.top();
-        S.pop();
-        prev = S.top();
-        S.push(now); // trick to get the 2nd item from top of S
-        if (ccw(prev, now, Polygon[i])) { // if these 3 points make a left turn
-            S.push(Polygon[i]); // accept
-            i++;
-        }
-        else // otherwise
-            S.pop(); // pop this point until we have a left turn
-    }
-    vector<point> ConvexHull;
-    while (!S.empty()) { // from stack back to vector
-        ConvexHull.push_back(S.top());
-        S.pop();
-    }
-    ConvexHull.pop_back(); // the last one is a duplicate of first one
-    return ConvexHull; // return the result
+int main() {
+    PT val[] = {PT(0, 0), PT(1, 1), PT(2, 2), PT(-1, 0)};
+    vector<PT> puntuak;
+    for (int i = 0; i < 4; i++)
+        puntuak.push_back(val[i]);
+    
+    ConvexHull(puntuak);
+    for (int i = 0; i < puntuak.size(); i++)
+        cout << puntuak[i].x << " " << puntuak[i].y << endl;
+    return 0;   
 }
